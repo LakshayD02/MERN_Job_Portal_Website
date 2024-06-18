034c440b00068088fa8f2bac0bb15d26a12f4b1b
LAKSHAY DHOUNDIYAL <104713661+LakshayD02@users.noreply.github.com>
2024-06-18 14:05:08 +0530

    Update UpdateJob.jsx

--- job-portal-client/src/assets/Pages/UpdateJob.jsx
+++ job-portal-client/src/assets/Pages/UpdateJob.jsx
@@ -19,7 +19,7 @@ const UpdateJob = () => {
       const onSubmit = (data) => {
         data.skills = selectedOption;
         // console.log(data);
-        fetch(`http://localhost:3000/update-job/${id}`, {
+        fetch(`https://mern-job-portal-website.vercel.app/update-job/${id}`, {
           method: "PATCH",
           headers: {'content-type': 'application/json'},
           body: JSON.stringify(data)
@@ -31,7 +31,7 @@ const UpdateJob = () => {
             alert("Job Updated Successfully");
           }
           reset()
-          window.location.href = 'http://localhost:5173/my-job';
+          window.location.href = 'https://mern-job-portal-lakshay.vercel.app/my-job';
         });
         };
 
