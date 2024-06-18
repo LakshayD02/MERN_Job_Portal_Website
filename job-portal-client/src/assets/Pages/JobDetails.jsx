b19f302f30220bd05727b33cfc66f0484f3eed57
LAKSHAY DHOUNDIYAL <104713661+LakshayD02@users.noreply.github.com>
2024-06-18 14:02:41 +0530

    Update JobDetails.jsx

--- job-portal-client/src/assets/Pages/JobDetails.jsx
+++ job-portal-client/src/assets/Pages/JobDetails.jsx
@@ -8,7 +8,7 @@ const JobDetails = () => {
     const {id} = useParams();
     const [job, setJob] = useState([])
     useEffect(() => {
-        fetch(`http://localhost:3000/all-jobs/${id}`).then(res => res.json()).then(data => setJob(data))
+        fetch(`https://mern-job-portal-website.vercel.app/all-jobs/${id}`).then(res => res.json()).then(data => setJob(data))
     }, [])
 
     const handleApply = async() => {
