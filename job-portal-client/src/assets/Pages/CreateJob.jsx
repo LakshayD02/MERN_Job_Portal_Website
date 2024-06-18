253603e4db9628399bc0666ca332ada398dee980
LAKSHAY DHOUNDIYAL <104713661+LakshayD02@users.noreply.github.com>
2024-06-18 14:01:32 +0530

    Update CreateJob.jsx

--- job-portal-client/src/assets/Pages/CreateJob.jsx
+++ job-portal-client/src/assets/Pages/CreateJob.jsx
@@ -13,7 +13,7 @@ const CreateJob = () => {
       const onSubmit = (data) => {
         data.skills = selectedOption;
         // console.log(data);
-        fetch("http://localhost:3000/post-job", {
+        fetch("https://mern-job-portal-website.vercel.app/post-job", {
           method: "POST",
           headers: {'content-type': 'application/json'},
           body: JSON.stringify(data)
